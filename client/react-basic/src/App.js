6651f202588e4fd29e884f21535d21b76a6bd9c0
mototoke <mototoke@hogehoge.com>
2020-06-23 00:07:16 +0900

    Add Basic1・2, BasicUseEffect, Timer,TimerComponent, ApiFetch

--- client/react-basic/src/App.js
+++ client/react-basic/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import Basic1 from "./components/Basic1";
+// import Basic2 from "./components/Basic2";
+// import BasicUseEffect from "./components/BasicUseEffect";
+// import TimerContainer from "./components/TimerContainer";
+import ApiFetch from "./components/ApiFetch";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-
+        {/* <Basic2 /> */}
+        {/* <BasicUseEffect /> */}
+        {/* <TimerContainer /> */}
+        <ApiFetch />
       </header>
     </div>
   );
